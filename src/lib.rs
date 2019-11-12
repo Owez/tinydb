c3fdc5b41276e01b2575e87591a1d336e6dd2c70
scOwez <owez@scalist.net>
2019-11-12 11:46:17 +0000

    Basic db dumping

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,10 @@
 //! export option for the database, allowing for embedded databases.
 
 use std::collections::HashSet;
+use std::fs::File;
 use std::hash;
 use std::path::PathBuf;
+use std::io::prelude::*;
 
 #[derive(Debug)]
 pub enum DatabaseError {
@@ -19,6 +21,13 @@ pub enum DatabaseError {
     /// A duplicate value was found when adding to the database with
     /// [Database::allow_dupes] disallowed.
     DupeFound,
+    /// When [Database::save_path] is required but is not found. This commonly
+    /// happens when loading or dumping a database with [Database::save_path]
+    /// being [Option::None].
+    SavePathRequired,
+
+    /// Misc [std::io::Error] that could not be properly handled.
+    IOError(std::io::Error),
 }
 
 pub struct Database<T: hash::Hash + Eq> {
@@ -79,9 +88,46 @@ impl<T: hash::Hash + Eq> Database<T> {
     /// Dumps database to a new `.tinydb` file.
     ///
     /// Please ensure that [Database::save_path] is valid before using this.
-    pub fn dump_db(&self) {
-        unimplemented!();
+    pub fn dump_db(&self) -> Result<(), DatabaseError>{
+        let u8_dump: &[u8] = unsafe { any_as_u8_slice(self) };
+
+        let mut dump_file = self.open_db_path()?;
+
+        io_to_dberror(dump_file.write_all(u8_dump))?;
+
+        Ok(())
     }
+
+    /// Opens the path given in [Database::save_path] or returns a [DatabaseError].
+    fn open_db_path(&self) -> Result<File, DatabaseError> {
+        let definate_path = path_to_dberror(self.save_path.as_ref())?;
+
+        match File::open(&definate_path) {
+            Ok(x) => Ok(x),
+            Err(_) => Ok(io_to_dberror(File::create(&definate_path))?),
+        }
+    }
+}
+
+/// Converts a possible [std::io::Error] to a [DatabaseError].
+fn io_to_dberror<T>(io_res: Result<T, std::io::Error>) -> Result<T, DatabaseError> {
+    match io_res {
+        Ok(x) => Ok(x),
+        Err(e) => Err(DatabaseError::IOError(e)),
+    }
+}
+
+/// Converts an [Option]<[PathBuf]> into a [Result]<[PathBuf], [DatabaseError]>.
+fn path_to_dberror(path: Option<&PathBuf>) -> Result<PathBuf, DatabaseError> {
+    match path {
+        None => Err(DatabaseError::SavePathRequired),
+        Some(x) => Ok(x.to_owned()),
+    }
+}
+
+/// Converts a [Sized] generic to a u8 slice.
+unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
+    ::std::slice::from_raw_parts((p as *const T) as *const u8, ::std::mem::size_of::<T>())
 }
 
 #[cfg(test)]
@@ -95,8 +141,9 @@ mod tests {
         age: i32,
     }
 
+    /// Tests addition to in-memory db
     #[test]
-    fn db_add() -> Result<(), DatabaseError> {
+    fn item_add() -> Result<(), DatabaseError> {
         let mut my_db = Database::new(String::from("Adding test"), None, true);
 
         my_db.add_item(DemoStruct {
@@ -107,8 +154,9 @@ mod tests {
         Ok(())
     }
 
+    /// Tests removal from in-memory db
     #[test]
-    fn db_remove() -> Result<(), DatabaseError> {
+    fn item_remove() -> Result<(), DatabaseError> {
         let mut my_db = Database::new(String::from("Adding test"), None, true);
 
         let testing_struct = DemoStruct {
@@ -118,6 +166,27 @@ mod tests {
 
         my_db.add_item(&testing_struct)?;
         my_db.remove_item(&testing_struct)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn db_dump() -> Result<(), DatabaseError> {
+        let mut my_db = Database::new(String::from("Adding test"), Some(PathBuf::from("db/hi.tinydb")), true);
+        let testing_struct = DemoStruct {
+            name: String::from("Xander"),
+            age: 33,
+        };
+
+        let other = DemoStruct {
+            name: String::from("John"),
+            age: 54
+        };
+
+        my_db.add_item(testing_struct)?;
+        my_db.add_item(other)?;
+
+        my_db.dump_db()?;
 
         Ok(())
     }
